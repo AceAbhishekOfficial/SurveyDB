cd08b8d53b557c7be8a6275c154469d3c748be37
AceAbhishekOfficial <abhisheksriv046@gmail.com>
2025-09-08 03:38:42 +0530

    Allowed cors

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from fastapi import FastAPI
 from database import Base, engine
 from api import person_controller
 from api import auth_controller
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Village Survey API")
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 app.include_router(auth_controller.router)
 app.include_router(person_controller.router)
